516c42fd01442508455a1735bc9dcf9df2e4cd7f
Brunn0B <brito.brunnob@gmail.com>
2025-03-14 19:31:33 -0300

    ATT

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,12 +1,26 @@
+const CACHE_NAME = 'resolve-plus-cache-v1';
+const urlsToCache = [
+  '/',
+  '/TelaInicio.html',
+  '/EstiloGlobal.css',
+  '/main.js',
+  '/img/Logo Novo.png'
+];
+
 self.addEventListener('install', (event) => {
-    console.log('Service Worker instalado');
-  });
-  
-  self.addEventListener('fetch', (event) => {
-    event.respondWith(
-      caches.match(event.request).then((response) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
         return response || fetch(event.request);
       })
-    );
-  });
-  
+  );
+});
